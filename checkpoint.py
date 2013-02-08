fd7dc1288c0cd04618fb070455be18ddfe1467eb
leo-bogert <github@leo.bogert.de>
2013-02-08 03:30:59 +0100

    Normalize the input/output directory. This converts them to absolute paths and strips trailing slashes.

--- checkpoint.py
+++ checkpoint.py
@@ -2,14 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+from os import path
 
 class Checkpoint:
 	input_dir = None # The directory for which the Checkpoint is generated
 	output_dir = None # The directory to which the Checkpoint will be written
 
 	def __init__(self, input_dir, output_dir):
-		self.input_dir, self.output_dir = input_dir, output_dir
-
+		self.input_dir, self.output_dir = map(path.abspath, (input_dir, output_dir))
+		
 	class Entry:
 		path = None # The path of the file/directory in the filesystem
 		sha256sum = None # The sha256sum. None if the Entry is a directory
