1b090076c08627e66edb5cb038c25bb336defcd0
leo-bogert <github@leo.bogert.de>
2013-02-08 02:53:05 +0100

    Implement parsing of command line arguments. The nice thing is that the Python argparse module will automatically ensure that all arguments are present and add an optional --help argument.

--- checkpoint.py
+++ checkpoint.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
+
 class Checkpoint:
+	input_dir = None # The directory for which the Checkpoint is generated
+	output_dir = None # The directory to which the Checkpoint will be written
+
+	def __init__(self, input_dir, output_dir):
+		self.input_dir, self.output_dir = input_dir, output_dir
+
 	class Entry:
 		path = None # The path of the file/directory in the filesystem
 		sha256sum = None # The sha256sum. None if the Entry is a directory
@@ -19,7 +27,12 @@ class Checkpoint:
 	entries = None	# All entries
 
 def main():
-	pass
+	parser = argparse.ArgumentParser()
+	parser.add_argument("input_directory", help="The directory for which to generate the checkpoint")
+	parser.add_argument("output_directory", help="The directory to which the checkpoint shall be written. Will be created automatically if it does not exist")
+	args = parser.parse_args()
+	
+	checkpoint = Checkpoint(args.input_directory, args.output_directory)
 
 if __name__ == "__main__":
 	main()
