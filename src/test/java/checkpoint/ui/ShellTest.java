a09b9433d62a6f6bb973bcf3bbd10700b4e5ecac
leo-bogert <code@leo.bogert.de>
2019-10-14 19:20:17 +0200

    Tests: Add skeleton test for class Shell

---
+++ src/test/java/checkpoint/ui/ShellTest.java
@@ -0,0 +1,13 @@
+package checkpoint.ui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public final class ShellTest {
+
+	@Test public void testMain() {
+		Shell.main(new String[0]);
+	}
+
+}
