4aa8b7fa59ac6e2fac3e4e8adfe9d47d8dd9f950
leo-bogert <code@leo.bogert.de>
2019-10-16 01:29:07 +0200

    Datamodel: Add abstract class Timestamps

---
+++ src/main/java/checkpoint/datamodel/Timestamps.java
@@ -0,0 +1,24 @@
+package checkpoint.datamodel;
+
+import java.nio.file.attribute.FileTime;
+
+/** Filesystem timestamps of a file/directory.
+ *  Implementations of this class must read all timestamps at once from disk
+ *  upon instantiation to avoid unnecessary disk seeking. */
+public abstract class Timestamps {
+
+	public abstract FileTime getAccessTime();
+
+	/** Always returns null currently because:
+	 *  - the Linux kernel does not currently support obtaining it in userspace.
+	 *  - and Java 11 does not seem to support detection if the above is still
+	 *    the case, it will just return a different one of the timestamps. */
+	public final FileTime getBirthTime() {
+		return null;
+	}
+
+	public abstract FileTime getChangeTime();
+
+	public abstract FileTime getModificationTime();
+
+}
