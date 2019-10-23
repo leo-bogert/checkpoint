bd598adcbc8245e340fb3a757dcbfe2f338b4f16
leo-bogert <code@leo.bogert.de>
2019-10-24 00:11:33 +0200

    ICheckpoint.addNode(): Document why we check if the node is already contained

--- src/main/java/checkpoint/datamodel/ICheckpoint.java
+++ src/main/java/checkpoint/datamodel/ICheckpoint.java
@@ -6,7 +6,14 @@ import java.nio.file.Path;
 public interface ICheckpoint {
 
 	/** @throws IllegalArgumentException If a node with the given
-	 *     {@link INode#getPath()} is already contained . */
+	 *     {@link INode#getPath()} is already contained.
+	 *     The additional execution time for checking this instead of relying
+	 *     upon callers to be bug-free is acceptable because:
+	 *     - implementations shall use a data structure which provides implicit
+	 *       sorting since {@link #save(Path)} will need sorting anyway
+	 *     - and such data structures will usually also determine if the given
+	 *       element is already contained when trying to add it, so that
+	 *       information is available for free anyway. */
 	void addNode(INode n) throws IllegalArgumentException;
 
 	void save(Path checkpointDir) throws IOException;
