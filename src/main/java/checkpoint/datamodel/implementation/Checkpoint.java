d6858677fd889cf2cae4a1ebe8ae43bce2d9736d
leo-bogert <code@leo.bogert.de>
2019-10-24 00:29:22 +0200

    Checkpoint: Add storage for INodes

--- src/main/java/checkpoint/datamodel/implementation/Checkpoint.java
+++ src/main/java/checkpoint/datamodel/implementation/Checkpoint.java
@@ -2,11 +2,27 @@ package checkpoint.datamodel.implementation;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.TreeMap;
 
 import checkpoint.datamodel.ICheckpoint;
 import checkpoint.datamodel.INode;
 
 public final class Checkpoint implements ICheckpoint {
+
+	/** Storage of the {@link INode}s which have been added via
+	 *  {@link #addNode(INode)}.
+	 *  Beyond storage this also has the purpose of implicitly sorting them by
+	 *  their {@link INode#getPath()} since our human readable file format
+	 *  of {@link #save(Path)} will sort by path.
+	 * 
+	 *  FIXME: Code quality: Provide an explicit {@link Comparator} because
+	 *  {@link Path#compareTo(Path)}'s JavaDoc says the ordering is platform
+	 *  specific. 
+	 *  FIXME: Performance: Replace with data structure which supports fast
+	 *  concurrent adding so our many generator threads can deal with the
+	 *  sorting in parallel. */
+	private final TreeMap<Path, INode> nodes = new TreeMap<>();
 
 	@Override public void addNode(INode n) throws IllegalArgumentException {
 		throw new UnsupportedOperationException("FIXME: Implement!");
