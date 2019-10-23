cc05377e276832f1c5b04c1be895cc900ee732e8
leo-bogert <code@leo.bogert.de>
2019-10-23 23:32:46 +0200

    Add skeleton class Checkpoint
    Generated using Eclipse.

---
+++ src/main/java/checkpoint/datamodel/implementation/Checkpoint.java
@@ -0,0 +1,23 @@
+package checkpoint.datamodel.implementation;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import checkpoint.datamodel.ICheckpoint;
+import checkpoint.datamodel.INode;
+
+public final class Checkpoint implements ICheckpoint {
+
+	@Override public void addNode(INode n) throws IllegalArgumentException {
+		throw new UnsupportedOperationException("FIXME: Implement!");
+	}
+
+	@Override public void save(Path checkpointDir) throws IOException {
+		throw new UnsupportedOperationException("FIXME: Implement!");
+	}
+
+	@Override public ICheckpoint load(Path checkpointDir) throws IOException {
+		throw new UnsupportedOperationException("FIXME: Implement!");
+	}
+
+}
