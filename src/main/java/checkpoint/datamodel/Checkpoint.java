b09b5142f8fd44f2c0dec96fa896ccf7d671c385
leo-bogert <code@leo.bogert.de>
2019-10-23 19:28:52 +0200

    Checkpoint interface: Remove function which we won't need yet
    Will only be needed once we support filtering checkpoints - the initial
    implementation won't do so.

--- src/main/java/checkpoint/datamodel/Checkpoint.java
+++ src/main/java/checkpoint/datamodel/Checkpoint.java
@@ -10,8 +10,6 @@ public interface Checkpoint {
 	 *     {@link Node#getPath()} is already contained . */
 	void addNode(Node n) throws IllegalArgumentException;
 
-	Collection<Node> getNodes();
-
 	Checkpoint load(Path checkpointDir) throws IOException;
 
 	void save(Path checkpointDir) throws IOException;
