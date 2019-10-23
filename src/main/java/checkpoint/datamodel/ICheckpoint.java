883d377b863a6219857b2dfe4d765c3d3a365390
leo-bogert <code@leo.bogert.de>
2019-10-23 19:42:59 +0200

    Datamodel: Rename interface Checkpoint to ICheckpoint
    We will only have a single implementation of all classes in the datamodel
    so there is no good pre-/suffix to distinguish the implementations via
    their class names and thus the interfaces must be distinguished instead.
    I.e. the class implementing this interface will be called "Checkpoint"
    because there is no reasonable different name.

--- src/main/java/checkpoint/datamodel/ICheckpoint.java
+++ src/main/java/checkpoint/datamodel/ICheckpoint.java
@@ -3,13 +3,13 @@ package checkpoint.datamodel;
 import java.io.IOException;
 import java.nio.file.Path;
 
-public interface Checkpoint {
+public interface ICheckpoint {
 
 	/** @throws IllegalArgumentException If a node with the given
 	 *     {@link Node#getPath()} is already contained . */
 	void addNode(Node n) throws IllegalArgumentException;
 
-	Checkpoint load(Path checkpointDir) throws IOException;
+	ICheckpoint load(Path checkpointDir) throws IOException;
 
 	void save(Path checkpointDir) throws IOException;
 
