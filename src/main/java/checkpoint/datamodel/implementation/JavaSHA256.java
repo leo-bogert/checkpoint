5feb174828ccdb001d880b18532a267ebb1e35b8
leo-bogert <code@leo.bogert.de>
2019-10-24 20:01:21 +0200

    JavaSHA256: Add constructor

--- src/main/java/checkpoint/datamodel/implementation/JavaSHA256.java
+++ src/main/java/checkpoint/datamodel/implementation/JavaSHA256.java
@@ -9,6 +9,12 @@ import checkpoint.datamodel.ISHA256;
 /** Implements {@link ISHA256} using Java's default SHA256 implementation. */
 public final class JavaSHA256 implements ISHA256 {
 
+	private final byte[] sha256;
+
+	private JavaSHA256(byte[] sha256) {
+		this.sha256 = sha256;
+	}
+
 	@Override public JavaSHA256 sha256fromFile(Path p)
 			throws FileNotFoundException, IOException, InterruptedException {
 		
