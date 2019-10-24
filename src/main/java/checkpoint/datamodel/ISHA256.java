31f3c1ec015971db95598266d8db466fbd389577
leo-bogert <code@leo.bogert.de>
2019-10-24 20:06:55 +0200

    ISHA256: Fix interface methods wrongly being non-static

--- src/main/java/checkpoint/datamodel/ISHA256.java
+++ src/main/java/checkpoint/datamodel/ISHA256.java
@@ -1,13 +1,12 @@
 package checkpoint.datamodel;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Path;
-
 import checkpoint.datamodel.implementation.JavaSHA256;
 
 /** Provides the same hash values as GNU coreutils' sha256sum command with the
  *  '--binary' flag.
+ *  
+ *  Java does not support static abstract interface functions so there are
+ *  commented-out functions which are also required to be implemented.
  *  
  *  Implemented at {@link JavaSHA256} using Java's SHA256 implementation.
  *  TODO: Performance: Provide alternate implementations and benchmark which one
@@ -17,8 +16,10 @@ public interface ISHA256 {
 	/** Must obey {@link Thread#isInterrupted()} by throwing
 	 *  {@link InterruptedException} because we will use it upon arbitrarily
 	 *  large user-supplied files. */
-	ISHA256 sha256fromFile(Path p)
+	/*
+	static ISHA256 sha256fromFile(Path p)
 		throws FileNotFoundException, IOException, InterruptedException;
+	 */
 
 	// FIXME: Implement using https://stackoverflow.com/a/9655275
 	/** Returns a hex-encoded string which can be decoded using
@@ -26,6 +27,8 @@ public interface ISHA256 {
 	String toString();
 
 	/** @throws NumberFormatException If the hex encoding is not valid. */
-	ISHA256 sha256fromString(String hexEncoded) throws NumberFormatException;
-
+	/*
+	static ISHA256 sha256fromString(String hexEncoded)
+		throws NumberFormatException;
+	 */
 }
