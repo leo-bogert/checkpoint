2b33f7e838816d1ca277bd7e8ced5678eb82a613
leo-bogert <code@leo.bogert.de>
2019-10-26 06:11:56 +0200

    JavaSHA256: Add skeleton unit test for it

---
+++ src/test/java/checkpoint/datamodel/implementation/JavaSHA256Test.java
@@ -0,0 +1,30 @@
+package checkpoint.datamodel.implementation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/** Tests {@link JavaSHA256}. */
+public final class JavaSHA256Test {
+
+	@Test public void testSha256ofFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testSha256fromString() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+}
