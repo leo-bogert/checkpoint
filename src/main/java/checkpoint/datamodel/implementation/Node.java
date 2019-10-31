36744de430baca4982cd684021345e0a68c7a1d8
leo-bogert <code@leo.bogert.de>
2019-10-31 17:58:26 +0100

    Add skeleton class Node to implement interface INode

---
+++ src/main/java/checkpoint/datamodel/implementation/Node.java
@@ -0,0 +1,23 @@
+package checkpoint.datamodel.implementation;
+
+import java.nio.file.Path;
+
+import checkpoint.datamodel.INode;
+import checkpoint.datamodel.ISHA256;
+import checkpoint.datamodel.ITimestamps;
+
+public final class Node implements INode {
+
+	@Override public Path getPath() {
+		throw new UnsupportedOperationException("FIXME: Implement!");
+	}
+
+	@Override public ISHA256 getHash() {
+		throw new UnsupportedOperationException("FIXME: Implement!");
+	}
+
+	@Override public ITimestamps getTimetamps() {
+		throw new UnsupportedOperationException("FIXME: Implement!");
+	}
+
+}
