dd66c6e55e766f2916e55e3b42b422e76e81315e
leo-bogert <code@leo.bogert.de>
2019-10-31 18:23:08 +0100

    INode: Provide support for returning null for the hash / timestamps if computing them failed

--- src/main/java/checkpoint/datamodel/INode.java
+++ src/main/java/checkpoint/datamodel/INode.java
@@ -6,14 +6,19 @@ import java.nio.file.Path;
  *  Either a file or a directory on disk. */
 public interface INode {
 
+	/** Must never be null. */
 	Path getPath();
 
 	/** Returns null for directories.
-	 * 
 	 *  TODO: Actually do return a hash by hashing the hashes of all files and
-	 *  directories inside of it. */
+	 *  directories inside of it.
+	 *  
+	 *  For non-directories may return null if computing the ISHA256 failed
+	 *  due to e.g. IOException. */
 	ISHA256 getHash();
 
+	/** May return null if reading the ITimestamps failed due to e.g.
+	 *  IOException. */
 	ITimestamps getTimetamps();
 
 }
