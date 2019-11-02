02e6f5385fdb0a5e42a9c3bbf2b993441d38f310
leo-bogert <code@leo.bogert.de>
2019-11-02 02:27:57 +0100

    Add skeleton unit test for class Checkpoint

---
+++ src/test/java/checkpoint/datamodel/implementation/CheckpointTest.java
@@ -0,0 +1,22 @@
+package checkpoint.datamodel.implementation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public final class CheckpointTest {
+
+	@Test public void testAddNode() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test public void testLoad() {
+		// FIXME: Test for filenames which contain \n, \r\n and \t.
+		fail("Not yet implemented");
+	}
+
+}
