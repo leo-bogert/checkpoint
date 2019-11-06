c86f24a705e42872b6205b04443f69b19fbe6c6c
leo-bogert <code@leo.bogert.de>
2019-11-06 17:08:32 +0100

    INodeFinder: Fix accuracy of sample shell command

--- src/main/java/checkpoint/datamodel/INodeFinder.java
+++ src/main/java/checkpoint/datamodel/INodeFinder.java
@@ -10,7 +10,7 @@ public interface INodeFinder {
 
 	/** The set and Paths of the resulting INodes must be equal to the shell
 	 *  command:
-	 *      cd inputDir ; find . -mount \( -type f -o -type d \) -print0
+	 *      cd inputDir && find . -mount \( -type f -o -type d \) -print0
 	 *   
 	 *  (The paths do not have to end with a null-byte: The -print0 is merely
 	 *  included here to not spread dangerous shell commands which wouldn't work
