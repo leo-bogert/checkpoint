8aacf28397b09a7b1ad1ffcb9b1306664bd904b6
leo-bogert <code@leo.bogert.de>
2019-11-06 19:52:29 +0100

    ICheckpointGenerator: Refer to interfaces instead of their implementation

--- src/main/java/checkpoint/generation/ICheckpointGenerator.java
+++ src/main/java/checkpoint/generation/ICheckpointGenerator.java
@@ -2,14 +2,14 @@ package checkpoint.generation;
 
 import java.io.IOException;
 
+import checkpoint.datamodel.ICheckpoint;
 import checkpoint.datamodel.INode;
-import checkpoint.datamodel.implementation.Checkpoint;
 
 /** TODO: Specify constructors. */
 public interface ICheckpointGenerator {
 
 	/** Searches for input files and directories on disk, computes their
-	 *  {@link INode} data, and writes the {@link Checkpoint} consisting of all
+	 *  {@link INode} data, and writes the {@link ICheckpoint} consisting of all
 	 *  nodes to disk.
 	 *  
 	 *  @throws InterruptedException If shutdown has been requested by
