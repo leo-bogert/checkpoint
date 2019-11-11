1dc5c0405bdc95645b596ea636a6e9751afcd47c
leo-bogert <code@leo.bogert.de>
2019-11-11 19:12:37 +0100

    CreateCommand: Add skeleton for new options --threads and --buffer using JCommander

--- src/main/java/checkpoint/ui/shell/CreateCommand.java
+++ src/main/java/checkpoint/ui/shell/CreateCommand.java
@@ -6,14 +6,34 @@ import java.io.IOException;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import checkpoint.generation.ConcurrentCheckpointGenerator;
 
 final class CreateCommand extends Command {
 
 	@Override String getShortSyntax() {
 		return '\t' + getCommandName() + " INPUT_DIR OUTPUT_CHECKPOINT_DIR";
+
+	private class Options {
+		@Parameter(names = { "--threads" }, order = 0, description =
+			  "Number of threads to process files/directories with. "
+			+ "Each thread will use as much memory as given via --buffer. "
+			+ "Must be at least 1.")
+		private int threads = 1024;
+
+		@Parameter(names = { "--buffer" }, order = 1, description =
+			  "I/O buffer per thread, in bytes. Must at least 4096. "
+			+ "Making it divisible by 4096 is a good idea.")
+		private int buffer = 1024*1024;
+
+		@Parameter(description =
+			"INPUT_DIR OUTPUT_CHECKPOINT_DIR")
+		private List<String> args = new ArrayList<>(2);
 	}
 
 	@Override int run(List<String> args) {
