3802f6d6758f71fa387926779b0d8d1d2ffc0f5a
leo-bogert <code@leo.bogert.de>
2019-11-11 21:02:13 +0100

    Shell: Add FIXME of using JCommander everywhere now that it has proven to work as I desire

--- src/main/java/checkpoint/ui/shell/Shell.java
+++ src/main/java/checkpoint/ui/shell/Shell.java
@@ -25,6 +25,8 @@ public final class Shell {
 	private static final Map<String, Command> commandMap
 		= Command.getCommandMap(commandClasses);
 
+	// FIXME: Replace args processing with JCommander, CreateCommand already
+	// uses it and it seems like a nice library.
 	public static void main(String[] args) {
 		Command command;
 		
