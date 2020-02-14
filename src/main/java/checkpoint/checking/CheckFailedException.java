c848425c3abc680b35ddec69992e59c5deb4b2ed
leo-bogert <code@leo.bogert.de>
2020-02-14 22:15:22 +0100

    CheckFailedException: Clarify on how to disable stack trace generation
    https://stackoverflow.com/questions/11434431/exception-without-stack-trace-in-java

--- src/main/java/checkpoint/checking/CheckFailedException.java
+++ src/main/java/checkpoint/checking/CheckFailedException.java
@@ -6,9 +6,15 @@ package checkpoint.checking;
 final class CheckFailedException extends Exception {
 
 	CheckFailedException(String message) {
-		// TODO: Performance: Check if omitting the stack trace by using an
+		// TODO: Performance: Check if omitting the stack trace by using this
 		// appropriate super constructor yields any noticeable benefit for
-		// checkpoints with many failed checks.
+		// checkpoints with many failed checks:
+		/*     super(message, null, true, false); */
+		// Notice: Perhaps overriding fillInStackTrace() is required instead
+		// because fillInStackTrace() contains an alternate code path which
+		// may still run even if we tell the above constructor that the
+		// stack trace is not writable - I did not check in detail what runs
+		// that code path.
 		super(message);
 	}
 
