5610f34235ebd48884e5b953b58522042076398f
leo-bogert <code@leo.bogert.de>
2020-02-19 22:25:13 +0100

    MatureINodeChangedCheck: Add simple summary of purpose to JavaDoc

--- src/main/java/checkpoint/checking/checks/MatureINodeChangedCheck.java
+++ src/main/java/checkpoint/checking/checks/MatureINodeChangedCheck.java
@@ -5,7 +5,13 @@ import checkpoint.checking.ICheck;
 import checkpoint.datamodel.ICheckpoint;
 import checkpoint.datamodel.INode;
 
-/** Checks only "mature" {@link INode}s.
+/** Checks for directories and files on which you haven't worked for a long time
+ *  being modified, which indicates something tampered with them as you're
+ *  likely to not change them anymore if you stopped changing them for months at
+ *  some point.
+ *  
+ *  In more precise words this means:  
+ *  Checks only "mature" {@link INode}s.  
  *  Those are such where {@link INode#getTimetamps()} indicates that it has not
  *  changed for a "long" time, where "long" defaults to
  *  {@link #DEFAULT_MATURE_TIME_IN_MONTHS} and may be specified by the user.  
