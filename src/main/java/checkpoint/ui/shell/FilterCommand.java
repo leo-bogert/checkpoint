95c7ecd0619b2afbd2599ba46b8e7750ade4159f
leo-bogert <code@leo.bogert.de>
2020-07-25 20:16:54 +0200

    FilterCommand: Add member class Options, based on CreateCommand.Options

--- src/main/java/checkpoint/ui/shell/FilterCommand.java
+++ src/main/java/checkpoint/ui/shell/FilterCommand.java
@@ -1,12 +1,37 @@
 package checkpoint.ui.shell;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.beust.jcommander.Parameter;
 final class FilterCommand extends Command {
 
 	@Override String getShortSyntax() {
 		return '\t' + getCommandName() +
 			" [options] INPUT_CHECKPOINT_DIR [OUTPUT_CHECKPOINT_DIR]";
+	}
+
+	private static final class Options {
+		@Parameter(names = { "--remove-timestamps" }, description =
+		    "Can be a combination of the letters 'a', 'b', 'c', 'm' without "
+		  + "any separators to remove the atime, btime and so on. "
+		  + "Example: '--remove-timestamps bc'.")
+		String removeTimestamps;
+
+		@Parameter(description = "INPUT_CHECKPOINT_DIR [OUTPUT_CHECKPOINT_DIR]")
+		List<String> args = new ArrayList<>(2);
+
+		void validate() throws IllegalArgumentException {
+			// TODO: As of 2019-11-11 with JCommander 1.71 @Parameter(arity = 2)
+			// doesn't work for unnamed parameters it seems so we check it
+			// manually, try again in some years.
+			if(args.size() < 1)
+				throw new IllegalArgumentException("Missing input dir!");
+			else if(args.size() > 2) {
+				throw new IllegalArgumentException(
+					"Too many/unknown arguments: " + args);
+			}
+		}
 	}
 
 	@Override int run(List<String> args) {
