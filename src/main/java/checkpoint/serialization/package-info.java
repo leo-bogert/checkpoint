2f0eb269f9ff71591db7c9978a97f9b54087d75c
leo-bogert <code@leo.bogert.de>
2020-08-08 20:12:05 +0200

    Add TODO for moving misplaced code

--- src/main/java/checkpoint/serialization/package-info.java
+++ src/main/java/checkpoint/serialization/package-info.java
@@ -1,2 +1,6 @@
-/** Classes to write a checkpoint from the in-memory model to disk. */
+/** Classes to write a checkpoint from the in-memory model to disk.
+ *  
+ *  TODO: Code quality: The code actually is in class
+ *  {@link checkpoint.datamodel.implementation.Checkpoint} currently, move it to
+ *  new classes in this package.*/
 package checkpoint.serialization;
