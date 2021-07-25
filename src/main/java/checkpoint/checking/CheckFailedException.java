c13ed5c01726280db8185f5c2a788b6ff8e78057
leo-bogert <code@leo.bogert.de>
2021-07-25 18:18:56 +0200

    CheckFailedException: Documentation

--- src/main/java/checkpoint/checking/CheckFailedException.java
+++ src/main/java/checkpoint/checking/CheckFailedException.java
@@ -1,10 +1,15 @@
 package checkpoint.checking;
 
+import checkpoint.datamodel.INode;
+
 /** Thrown when an {@link ICheck} fails.  
  *  The {@link #getMessage()} is suitable for printing to stderr. */
 @SuppressWarnings("serial")
 public final class CheckFailedException extends Exception {
 
+	/** @param message Must be suitable for printing to stderr. The path of the
+	 *      affected {@link INode} must not be included, it will be printed by
+	 *      the {@link CheckRunner}. */
 	public CheckFailedException(String message) {
 		// TODO: Performance: Check if omitting the stack trace by using this
 		// appropriate super constructor yields any noticeable benefit for
